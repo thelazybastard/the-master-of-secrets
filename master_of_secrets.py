346e2d5313d2b93e20ca7d710afd615017ed1751
Monish G. <256585194+thelazybastard@users.noreply.github.com>
2026-01-31 14:39:18 +0700

    Add files via upload

---
+++ master_of_secrets.py
@@ -0,0 +1,18 @@
+# TODO: password generator with entropy
+#  - ask users for input
+#  -
+
+import secrets
+import string
+
+def main():
+    get_user_input()
+
+def get_user_input():
+    print("Greetings, The Master of Secrets is at your service!")
+    user_input = input("Would you like to know a secret?")
+
+def generate_password():
+    password =
+
+main()
