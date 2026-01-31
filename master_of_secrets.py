d2d5ed45ad0ead507a1d7496f340f68759e0ba90
Monish G. <256585194+thelazybastard@users.noreply.github.com>
2026-01-31 15:32:52 +0700

    finished the generate_password function
    added if function for when users change their mind on generating password

--- master_of_secrets.py
+++ master_of_secrets.py
@@ -1,25 +1,30 @@
 # TODO: password generator with entropy
 #  - ask users for input - done
 #  - generates password - done
-#  - hashes password
+#  - generates chart to show password strength
 
 import secrets
 import string
-import hashlib
 
 def main():
-    get_user_input()
+    generate_password()
 
 def get_user_input():
     print("Greetings, The Master of Secrets is at your service!")
-    user_input = input("Would you like to know a secret?")
+    user_input = input("Would you like to know a secret? (Yes/No): ")
+    return user_input
 
 def generate_password():
+    user_input = get_user_input()
+    if user_input == "Yes":
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(secrets.choice(characters) for i in range(16)).encode()
+        return password
+    elif user_input == "No":
+        print ("Come back soon!")
+    return None
 
-    characters = string.ascii_letters + string.digits + string.punctuation
-
-    password = ''.join(secrets.choice(characters) for i in range(32))
-
-
+def display_strength():
+    pass
 
 main()
