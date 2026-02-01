f531d6ed1fbc9cd07ede7c7b210dccaabecf1ccc
Monish Giani <256585194+thelazybastard@users.noreply.github.com>
2026-02-01 20:07:02 +0700

    added entropy calculation
    used numpy and scipy

--- master_of_secrets.py
+++ master_of_secrets.py
@@ -5,6 +5,9 @@
 
 import secrets
 import string
+import numpy as np
+from scipy.stats import entropy
+
 
 def main():
     generate_password()
@@ -18,11 +21,20 @@ def generate_password():
     user_input = get_user_input()
     if user_input == "Yes":
         characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(secrets.choice(characters) for i in range(16)).encode()
+        password = ''.join(secrets.choice(characters) for i in range(20))
         return password
     elif user_input == "No":
         print ("Come back soon!")
     return None
+
+
+def calculate_password_entropy():
+    password = generate_password()
+    chars, counts = np.unique(list(password), return_counts=True)
+    probabilities = counts / len(password)
+    ent = entropy(probabilities, base=2)
+    return ent
+
 
 def display_strength():
     pass
