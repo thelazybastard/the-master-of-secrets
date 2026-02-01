0093b55f46be7137a3946b777903f54b17bbc285
Monish Giani <256585194+thelazybastard@users.noreply.github.com>
2026-02-01 20:20:00 +0700

    calculates time to crack password

--- master_of_secrets.py
+++ master_of_secrets.py
@@ -1,8 +1,3 @@
-# TODO: password generator with entropy
-#  - ask users for input - done
-#  - generates password - done
-#  - generates chart to show password strength
-
 import secrets
 import string
 import numpy as np
@@ -10,33 +5,46 @@ from scipy.stats import entropy
 
 
 def main():
-    generate_password()
+    display_strength()
 
 def get_user_input():
     print("Greetings, The Master of Secrets is at your service!")
+
     user_input = input("Would you like to know a secret? (Yes/No): ")
+
     return user_input
 
 def generate_password():
     user_input = get_user_input()
+
     if user_input == "Yes":
         characters = string.ascii_letters + string.digits + string.punctuation
         password = ''.join(secrets.choice(characters) for i in range(20))
         return password
     elif user_input == "No":
         print ("Come back soon!")
+
     return None
 
 
-def calculate_password_entropy():
-    password = generate_password()
+def calculate_entropy(password):
     chars, counts = np.unique(list(password), return_counts=True)
     probabilities = counts / len(password)
     ent = entropy(probabilities, base=2)
+
     return ent
 
 
-def display_strength():
-    pass
+def crack_time(entropy_bits, guesses_per_second=1e9):
+    total_combinations = 2 ** entropy_bits
+
+    avg_seconds = total_combinations / (2 * guesses_per_second)
+    seconds_per_year = 365.25 * 24 * 60 * 60
+    years = avg_seconds / seconds_per_year
+
+    return years
+
+
+
 
 main()
